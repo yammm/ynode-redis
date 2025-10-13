f6583036bbd467aa738c3d7330a54becc5f7ace4
Michael Welter <me@mikinho.com>
2025-10-13 09:10:44 -0500

    Add support for redis v4.
    Signed-off-by: Michael Welter <me@mikinho.com>

--- src/plugin.js
+++ src/plugin.js
@@ -98,7 +98,14 @@ export default fp(async function (fastify, opts) {
             return;
         }
         fastify.log.debug(`Attempting to close our Redis client [${info.id}] redis://${info.addr}`);
-        await client.close();
+        // node-redis v5: close(); v4: quit() / disconnect()
+        if (typeof client.close === "function") {
+            await client.close();
+        } else if (typeof client.quit === "function") {
+            await client.quit();
+        } else if (typeof client.destroy === "function" || typeof client.disconnect === "function") {
+            await (client.destroy?.() ?? client.disconnect());
+        }
     });
 }, {
     fastify: "5.x",
